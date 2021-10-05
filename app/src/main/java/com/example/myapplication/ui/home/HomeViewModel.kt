a5848c4717f5d7ac46dec4b241913bc2f9615f2b
Richard Shea <rshea@thecubagroup.com>
2021-10-05 14:29:51 +1300

    Tweak text on home page

--- app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Welcome to the home page of Richard Shea's android-studio-test"
     }
     val text: LiveData<String> = _text
 }
